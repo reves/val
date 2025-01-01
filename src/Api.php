e13100d56d7e4b779d831aa3f32190b0cceb7682
Constantin Miron <mc.reves@gmail.com>
2025-01-01 18:58:25 +0200

    api: allow respondData() for all request types

--- src/Api.php
+++ src/Api.php
@@ -33,9 +33,10 @@ Class Api
     private array $fields = [];
 
     /**
-     * Calls the requested API action method. Responds with an HTTP status code of 
-     * "404 Not Found" if the action method is private or does not exists. Sends the 
-     * fail response after calling the requested action method, if necessary.
+     * Calls the requested API action method. Responds with an HTTP status code
+     * of "404 Not Found" if the action method is private or does not exists.
+     * Sends the fail response after calling the requested action method, if
+     * necessary.
      */
     final public function __construct(?string $action = null)
     {
@@ -55,130 +56,9 @@ Class Api
     }
 
     /**
-     * Used only for a successful POST request. Sets the HTTP status code to "200 OK" 
-     * and exits the application.
-     * 
-     * @throws \LogicException
-     */
-    final protected function respondSuccess()
-    {
-        if (!$this->flagOnlyPOST)
-            throw new \LogicException('This type of response is valid only for the POST method.');
-        
-        http_response_code(200);
-        App::exit();
-    }
-
-    /**
-     * Used only for a successful GET request. Sets the HTTP status code to "200 OK", 
-     * sends the data and exits the application.
-     * 
-     * Follow the recommended response formats (which can be combined if necessary):
-     * 
-     *  a) E.g. a single post.
-     *      ["post" => ["id" => 1, "name" => "Qwerty"]]
-     * 
-     *  b) E.g. multiple posts.
-     *      ["posts" => [["id" => 1, "name" => "Qw"], ["id" => 2, "name" => "Er"], ...]]
-     * 
-     * @throws \LogicException
-     */
-    final protected function respondData(array $data)
-    {
-        if (!$this->flagOnlyGET)
-            throw new \LogicException('This type of response is valid only for the GET method.');
-        
-        http_response_code(200);
-        echo JSON::encode($data);
-        App::exit();
-    }
-
-    /**
-     * Used for unsuccessful request processing or invalid call conditions. Sets the 
-     * HTTP status code to "500 Internal Server Error" or "4xx Client Error" (except 
-     * 400) and exits the application. This type of response is valid for both GET and 
-     * POST methods.
-     * 
-     * @throws \InvalidArgumentException
-     */
-    final protected function respondError(int $code)
-    {
-        if ($code < 401 || $code > 500)
-            throw new \InvalidArgumentException('The "int $code" parameter value must be between 401 and 500 inclusive.');
-
-        http_response_code($code);
-        App::exit();
-    }
-
-    /**
-     * Attention (!) use this method only if a custom fail response is required. Check out 
-     * the standard methods setFail() and setWrong() first.
-     * 
-     * Used when there is a problem with the data provided, or if any precondition of 
-     * the call has not been met. Sets the HTTP status code to "400 Bad Request", sends 
-     * the response and exits the application.
-     * 
-     * Follow the recommended response formats (which can be combined if necessary):
-     * 
-     *  a) E.g. describe the reasons why data validation failed.
-     *      ['fail' => ['status' => "specific_status_code", 'description' => "Description"]]
-     * 
-     *  b) E.g. enumerate missing but required fields.
-     *      ['missing' => ["title", "url", "notes"]]
-     * 
-     *  c) E.g. describe invalid field values and their mandatory parameters.
-     *      ['wrong' => ["price" => ['status' => "specific_status_code", 'params' => ["minValue" => 1]]]]
-     * 
-     */
-    final protected function respondFail(array $response)
-    {
-        http_response_code(400);
-        echo JSON::encode($response);
-        App::exit();
-    }
-
-    /**
-     * Sets the specific status code and optional description which describe the reasons 
-     * why data validation failed. The See the documentation for the self::respondFail() 
-     * method for details and response examples.
-     */
-    final protected function setFail(string $status, ?string $description = null) : self
-    {
-        $this->fail = ['status' => $status];
-        if ($description) $this->fail['description'] = $description;
-        return $this;
-    }
-
-    /**
-     * Sets the description for the field whose value is not valid. The description 
-     * contains a specific status code and mandatory parameters for the field. See the 
-     * documentation for the self::respondFail() method for details and response examples.
-     */
-    final protected function setWrong(string $field, string $status, ?array $params = null) : self
-    {
-        $this->wrong[$field] = ['status' => $status];
-        if ($params) $this->wrong[$field]['params'] = $params;
-        return $this;
-    }
-
-    /**
-     * Sends a fail response, only if the request failed.
-     */
-    final protected function respondOnFail() : self
-    {
-        $response = [];
-
-        if ($this->fail) $response['fail'] = $this->fail;
-        if ($this->missing) $response['missing'] = $this->missing;
-        if ($this->wrong) $response['wrong'] = $this->wrong;
-        if ($response) $this->respondFail($response);
-
-        return $this;
-    }
-
-    /**
-     * Allows only GET requests to call the API action method. Responds with an HTTP 
-     * status code of "405 Method Not Allowed" if another request method is used.
+     * Allows only GET requests to call the API action method. Responds with an
+     * HTTP status code of "405 Method Not Allowed" if another request method
+     * is used.
      * 
      * @throws \LogicException
      */
@@ -197,9 +77,10 @@ Class Api
     }
 
     /**
-     * Allows only POST requests to call the API action method. Responds with an HTTP 
-     * status code of "403 Forbidden" if the CSRF Token is invalid. Responds with an 
-     * HTTP status code of "405 Method Not Allowed" if another request method is used.
+     * Allows only POST requests to call the API action method. Responds with
+     * an HTTP status code of "403 Forbidden" if the CSRF Token is invalid.
+     * Responds with an HTTP status code of "405 Method Not Allowed" if another
+     * request method is used.
      * 
      * @throws \LogicException
      */
@@ -223,8 +104,9 @@ Class Api
     }
     
     /**
-     * Allows only Authenticated user to call the API action method. Responds with an 
-     * HTTP status code of "401 Unauthorized" if user is not Authenticated.
+     * Allows only Authenticated user to call the API action method. Responds 
+     * with an HTTP status code of "401 Unauthorized" if user is not
+     * Authenticated.
      * 
      * @throws \LogicException
      */
@@ -243,8 +125,9 @@ Class Api
     }
 
     /**
-     * Allows only Unauthenticated user to call the API action method. Responds with an 
-     * HTTP status code of "403 Forbidden" if user is not Unauthenticated.
+     * Allows only Unauthenticated user to call the API action method. Responds
+     * with an HTTP status code of "403 Forbidden" if user is not
+     * Unauthenticated.
      * 
      * @throws \LogicException
      */
@@ -263,41 +146,32 @@ Class Api
     }
 
     /**
-     * Gets the value of the specified field.
-     * 
-     * @throws \InvalidArgumentException
-     */
-    final protected function val(string $field)
-    {
-        if (!isset($this->fields[$field]))
-            throw new \InvalidArgumentException('The "' . $field . '" field is not set, make sure it is specified as a required field.');
-
-        return $this->fields[$field];
-    }
-
-    /**
      * Sets the required fields for the requested API action method. Also calls 
-     * user-defined validation methods for the required fields. Sends a fail response 
-     * if any of the listed fields is missing or any of the listed fields has an 
-     * invalid value.
+     * user-defined validation methods for the required fields. Sends a fail
+     * response if any of the listed fields is missing or any of the listed 
+     * fields has an invalid value.
      * 
      * Validation method definition syntaxes:
      * 
-     *  a) Without a parameter. Inside the method function, use the $this->val('field') 
+     *  a) Without a parameter.
+     *  Inside the validation method function, use the $this->val('field') 
      *  construction to get the value of the field.
      * 
-     *      protected function validate<Fieldname>() {}
+     *      protected function validate<Fieldname>() {
+     *         $value = $this->val('<fieldname>');
+     *      }
      * 
      *  b) With a parameter containing the value of the field being validated.
      * 
      *      protected function validate<Fieldname>($value) {}
      * 
-     *  c) With a parameter containing a reference to the value of the field being 
-     *  validated. That is, the field value can be changed or modified.
+     *  c) With a parameter containing a reference to the value of the field
+     *  being validated. That is, the field value can be changed or modified.
      * 
      *      protected function validate<Fieldname>(&$value) {}
      * 
-     * ...where "<Fieldname>" is the name of the field whose value is being validated.
+     * ...where "<Fieldname>" is the name of the field whose value is being
+     * validated.
      * 
      */
     final protected function required(string ...$fields) : self
@@ -339,6 +213,147 @@ Class Api
         }
 
         return $this->respondOnFail();
+    }
+
+    /**
+     * Gets the value of the specified field.
+     * 
+     * @throws \InvalidArgumentException
+     */
+    final protected function val(string $field)
+    {
+        if (!isset($this->fields[$field]))
+            throw new \InvalidArgumentException('The "' . $field . '" field is not set, make sure it is specified as a required field.');
+
+        return $this->fields[$field];
+    }
+
+    /**
+     * Responds to a successfult request. Sets the HTTP status code to "200 OK"
+     * and exits the application.
+     */
+    final protected function respondSuccess()
+    {
+        http_response_code(200);
+        App::exit();
+    }
+
+    /**
+     * Responds with data to a successful request. Sets the HTTP status code to
+     * "200 OK", sends the data and exits the application.
+     * 
+     * Follow the recommended response formats (which can be combined if
+     * necessary):
+     * 
+     *  a) E.g. a single post:
+     *      ["post" => ["id" => 1, "name" => "Qwerty"]]
+     * 
+     *  b) E.g. multiple posts:
+     *      ["posts" => [
+     *          ["id" => 1, "name" => "Qw"],
+     *          ["id" => 2, "name" => "Er"], ...
+     *      ]]
+     * 
+     */
+    final protected function respondData(array $data)
+    {
+        http_response_code(200);
+        echo JSON::encode($data);
+        App::exit();
+    }
+
+    /**
+     * Used for unsuccessful request processing or invalid call conditions.
+     * Sets the HTTP status code to "500 Internal Server Error" (by default) or
+     * "4xx Client Error" (except 400) and exits the application.
+     * 
+     * @throws \InvalidArgumentException
+     */
+    final protected function respondError(int $code = 500)
+    {
+        if ($code < 401 || $code > 500)
+            throw new \InvalidArgumentException('The "int $code" parameter value must be between 401 and 500 inclusive.');
+
+        http_response_code($code);
+        App::exit();
+    }
+
+    /**
+     * Sends a response only in case there is a problem with the data provided,
+     * or if any precondition of the call has not been met. Sets the HTTP
+     * status code to "400 Bad Request", sends the response and exits the
+     * application.
+     * 
+     * The response format can be one of the following or a combination of them:
+     * 
+     *  a) From setFail() method:
+     *      ['fail' => [
+     *          'status' => "EXISTING_USERNAME",
+     *          'description' => "The username already exists."
+     *     ]]
+     * 
+     *  b) From setWrong() method:
+     *      ['wrong' => [
+     *          "price" => [
+     *              'status' => "custom_status_code",
+     *              'params' => ["minValue" => 1]
+     *         ],
+     *     ]]
+     * 
+     *  c) A list of missing but required fields, specified in the required()
+     *  method:
+     *      ['missing' => ["title", "url", "notes"]]
+     */
+    final protected function respondOnFail() : self
+    {
+        $response = [];
+
+        if ($this->fail) $response['fail'] = $this->fail;
+        if ($this->missing) $response['missing'] = $this->missing;
+        if ($this->wrong) $response['wrong'] = $this->wrong;
+
+        if ($response) {
+            http_response_code(400);
+            echo JSON::encode($response);
+            App::exit();
+        }
+
+        return $this;
+    }
+
+    /**
+     * Sets a custom status string and an optional description which describe
+     * the reasons why data validation failed for the current request.
+     * 
+     * Example:
+     * 
+     *      $this->setFail("EXISTING_USERNAME", "Username already exists.");
+     * 
+     */
+    final protected function setFail(string $status, ?string $description = null) : self
+    {
+        $this->fail = ['status' => $status];
+        if ($description) $this->fail['description'] = $description;
+        return $this;
+    }
+
+    /**
+     * Sets a custom status string and a list of mandatory parameters, for the
+     * specified field whose value is not valid.
+     * 
+     * Example:
+     * 
+     *      $this->setWrong("price", "INVALID_PRICE", [
+     *          "minValue" => 1,
+     *          "maxValue" => 10000
+     *      ]);
+     * 
+     */
+    final protected function setWrong(string $field, string $status, ?array $params = null) : self
+    {
+        $this->wrong[$field] = ['status' => $status];
+        if ($params) $this->wrong[$field]['params'] = $params;
+        return $this;
     }
 
     /**
